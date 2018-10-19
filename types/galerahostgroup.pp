f202462a7b10bc71db7ca46f001260be174f1fcb
Jakob Kartschall <j.kartschall@syseleven.de>
2018-10-19 15:19:15 +0200

    fixup! remove comma

--- types/galerahostgroup.pp
+++ types/galerahostgroup.pp
@@ -5,5 +5,5 @@ type Proxysql::GaleraHostgroup = Array[Hash[String, Struct[{ writer             
                                                                        offline                    => Integer,
                                                                        Optional[active]           => Integer[1],
                                                                        Optional[writers]          => Integer,
-                                                                       Optional[writer_is_reader] => Integer[1]
+                                                                       Optional[writer_is_reader] => Integer[1], }],1,1]]
 # lint:endignore
