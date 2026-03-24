30f7b359ddb9655225eba29706b07ab97971dca8
Sascha Doering <s.doering@syseleven.de>
2026-03-24 12:10:49 +0000

    atm-10492: sdoering: WIP: [ci skip]

--- lib/puppet/provider/proxy_mysql_user/proxysql.rb
+++ lib/puppet/provider/proxy_mysql_user/proxysql.rb
@@ -61,7 +61,7 @@ Puppet::Type.type(:proxy_mysql_user).provide(:proxysql, parent: Puppet::Provider
     frontend = @resource.value(:frontend) || 1
     max_connections = @resource.value(:max_connections) || 10_000
 
-    _password = (password[0, 1] == '*') ? '\'#{password}\'' : 'CACHING_SHA2_PASSWORD(\'#{password}\')'
+    _password = (password[0, 1] == '*') ? "'#{password}'" : "CACHING_SHA2_PASSWORD('#{password}')"
 
     query = 'INSERT INTO mysql_users (`username`, `password`, `active`, `use_ssl`, `default_hostgroup`, `default_schema`,  ' \
             '`schema_locked`, `transaction_persistent`, `fast_forward`, `backend`, `frontend`, `max_connections`)  ' \
